926a8ab8e2904d20bd33b3cd5ec0ed6af91db144
thwldus <gee906@naver.com>
2024-06-29 00:48:39 +0900

    [FEAT] 7주차-엔티티 클래스 추가

---
+++ src/main/java/umc/spring/domain/Mission.java
@@ -0,0 +1,28 @@
+package umc.spring.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer reward;
+
+    private LocalDate deadline;
+
+    private String missionSpec;
+}
