ae4b6d8d85bf56dccc2f897922dda771e20b7e0b
thwldus <gee906@naver.com>
2024-06-30 17:59:35 +0900

    [FEAT] 8주차-임시 API 만들어보기

---
+++ src/main/java/umc/spring/converter/TempConverter.java
@@ -0,0 +1,13 @@
+package umc.spring.converter;
+
+import umc.spring.web.dto.TempResponse;
+
+public class TempConverter {
+
+    public static TempResponse.TempTestDTO toTempTestDTO(){
+        return TempResponse.TempTestDTO.builder()
+                .testString("This is vacation study!")
+                .build();
+    }
+
+}
